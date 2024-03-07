91fd9741be6d4b0bf5c547e9c76350b8428d8615
Jaehui Yu <pkj0522@naver.com>
2024-03-07 16:55:48 +0900

    [feat] 검색 화면 UI 구현

---
+++ Yakssok/Search/SearchViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SearchViewController.swift
+//  Yakssok
+//
+//  Created by Jaehui Yu on 3/7/24.
+//
+
+import UIKit
+import SnapKit
+
+class SearchViewController: BaseViewController {
+    lazy var searchCollectionView = UICollectionView(frame: .zero, collectionViewLayout: configureUICollectionView())
+    
+    private func configureUICollectionView() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 16
+        let cellWidth = UIScreen.main.bounds.width - (spacing * 3)
+        layout.itemSize = CGSize(width: cellWidth / 2, height: cellWidth / 2)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        return layout
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(searchCollectionView)
+    }
+    
+    override func configureView() {
+        searchCollectionView.backgroundColor = .systemGray
+        searchCollectionView.delegate = self
+        searchCollectionView.dataSource = self
+        searchCollectionView.register(SearchResultCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    }
+    
+    override func configureConstraints() {
+        searchCollectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SearchResultCollectionViewCell
+        return cell
+    }
+}
