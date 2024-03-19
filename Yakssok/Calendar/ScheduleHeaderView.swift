560373c05e17a7db665eecd1ce0e07ffab7a6506
Jaehui Yu <pkj0522@naver.com>
2024-03-19 22:46:40 +0900

    [refactor] CalendarViewController 관련 코드 리팩토링

---
+++ Yakssok/Calendar/ScheduleHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleHeaderView.swift
+//  Yakssok
+//
+//  Created by Jaehui Yu on 3/19/24.
+//
+
+import UIKit
+
+class ScheduleHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
