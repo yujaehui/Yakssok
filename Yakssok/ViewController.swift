c32520397843c3e21997dd59a256d7f29e5aa359
Jaehui Yu <pkj0522@naver.com>
2024-03-19 18:19:59 +0900

    [feat] TabBar 연결

--- Yakssok/ViewController.swift
+++ Yakssok/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        addVC()
     }
 
-
+    private func addVC() {
+        let calenderVC = CalendarViewController()
+        calenderVC.tabBarItem = UITabBarItem(title: "calendar", image: UIImage(systemName: "calendar"), selectedImage: UIImage(systemName: "calendar"))
+        
+        let myVC = MyViewController()
+        myVC.tabBarItem = UITabBarItem(title: "my", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person"))
+        
+        self.viewControllers = [calenderVC, myVC]
+    }
 }
 
