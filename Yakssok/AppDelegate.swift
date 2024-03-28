e8186f4b8b7b1cec6df2c05b3f062b61f5faa7a9
Jaehui Yu <pkj0522@naver.com>
2024-03-28 14:46:30 +0900

    [feat] firebase Analytics, Crashlytics 적용

--- Yakssok/AppDelegate.swift
+++ Yakssok/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
             if granted {
                 print("Notification permission granted")
